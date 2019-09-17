2a60d8969e17e07cfe98962883caa36597b5f006
KhanhTran <khanhtran@gooappable.com>
2019-09-17 21:40:38 +0700

    update

---
+++ module2/Exersice1/typeanotation.ts
@@ -0,0 +1,18 @@
+function display(id:number, name:string)
+{
+    console.log("Id = " + id + ", Name = " + name);
+}
+
+var age: number = 32; // number variable
+var name: string = "John";// string variable
+var isUpdated: boolean = true;// Boolean variabl
+
+var employee : { 
+    id: number; 
+    name: string; 
+}; 
+
+employee = { 
+  id: 100, 
+  name : "John"
+}
